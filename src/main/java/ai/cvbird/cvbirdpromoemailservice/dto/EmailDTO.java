c7d8eb842522008d1f41ee5452246f82843fc9da
Емельянов Константин Валентинович <emelyanovkonstantin@yandex.ru>
2023-11-01 22:36:45 +0300

    Added TODO

--- src/main/java/ai/cvbird/cvbirdpromoemailservice/dto/EmailDTO.java
+++ src/main/java/ai/cvbird/cvbirdpromoemailservice/dto/EmailDTO.java
@@ -15,6 +15,10 @@ public class EmailDTO {
 
     Long id;
 
+    // TODO  strengthen the restrictions
+    // https://haacked.com/archive/2007/08/21/i-knew-how-to-validate-an-email-address-until-i.aspx/
+    //  @Email(message="Please provide a valid email address",
+    //  regexp = "^(?!\.)("([^"\r\\]|\\["\r\\])*"|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$")
     @Email(message="Please provide a valid email address", regexp = ".+[@].+[\\.].+")
     @UniqueEmail
     String email;
